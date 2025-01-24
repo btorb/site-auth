b44e371e73eda15c98069b09b7c576daef8eca98
Ben Torben-Nielsen <btorbennielsen@gmail.com>
2025-01-24 17:06:06 +0100

    ugh

--- api/auth.js
+++ api/auth.js
@@ -1,5 +1,10 @@
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
+    if (req.method === 'OPTIONS') { 
+        // Handle preflight requests
+        res.setHeader('Access-Control-Allow-Methods', 'POST'); // Allow POST requests
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow Content-Type header
+        res.status(200).end(); // Respond with OK
+    } else if (req.method === 'POST') {
       const { username, password } = req.body;
   
       // **IMPORTANT:** Replace these with your actual credentials
